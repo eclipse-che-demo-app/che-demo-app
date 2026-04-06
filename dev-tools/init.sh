102cd9fcf7a6b73b1c1722677f79a1f07040e32b
cgruver <cgruver@redhat.com>
2026-04-06 18:58:05 +0000

    remove deprecated image files

--- dev-tools/init.sh
+++
@@ -1,9 +0,0 @@
-oc new-project dev-tools  
-oc policy add-role-to-group system:image-puller system:serviceaccounts -n dev-tools
-oc apply -f ./tools-build.yaml
-
-oc import-image postgresql-15-c9s:c9s --from=quay.io/sclorg/postgresql-15-c9s:c9s --confirm -n dev-tools
-oc import-image ubi9-minimal:9.5 --from registry.access.redhat.com/ubi9-minimal:9.5 --confirm -n dev-tools
-
-oc start-build dev-tools -n dev-tools -w -F    
-
--- images/dev-tools.Containerfile
+++
@@ -1,5 +0,0 @@
-FROM scratch
-
-ARG TOOLS_DIR="./tools"
-
-COPY ${TOOLS_DIR}/ /tools
